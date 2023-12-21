415a75e7bf91976bbcaa19f04b89247ebcb893de
minneelyyyy <minneelyyyy@proton.me>
2023-12-20 21:23:09 -0500

    rewrite server to use tokio-rustls instead of plain rustls with mio

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,10 @@
-use std::io::Write;
-use std::net;
+
 use std::io;
 use std::fs;
 use std::sync::Arc;
+use tokio::net;
 
-use std::io::Read as _;
+use tokio::io::AsyncWriteExt;
 
 fn load_certs(filename: &str) -> Vec<pki_types::CertificateDer<'static>> {
     let certfile = fs::File::open(filename).expect("cannot open certificate file");
@@ -34,91 +34,38 @@ fn load_private_key(filename: &str) -> pki_types::PrivateKeyDer<'static> {
     );
 }
 
-fn main() -> io::Result<()> {
-    let mut listener = mio::net::TcpListener::bind("0.0.0.0:7700".parse().unwrap())
-        .expect("failed to bind addr");
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let addr: std::net::SocketAddr = "0.0.0.0:7700".parse().unwrap();
 
-    let suites = rustls::crypto::ring::ALL_CIPHER_SUITES.to_vec();
-    let versions = rustls::ALL_VERSIONS.to_vec();
+    let certs = load_certs("cert/server.crt");
+    let key = load_private_key("cert/server.key");
 
-    let client_cert_verifier = rustls::server::WebPkiClientVerifier::no_client_auth();
+    let config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
 
-    let cert_chain = load_certs("cert/server.crt");
-    let key_der = load_private_key("cert/server.key");
-
-    let config = Arc::new(rustls::ServerConfig::builder_with_provider(
-            rustls::crypto::CryptoProvider {
-                cipher_suites: suites,
-                ..rustls::crypto::ring::default_provider()
-            }
-            .into()
-        ).with_protocol_versions(&versions)
-        .expect("inconsistent cipher-suites/versions specified")
-        .with_client_cert_verifier(client_cert_verifier)
-        .with_single_cert(cert_chain, key_der)
-        .expect("bad certificates/private key"));
-
-    let mut poll = mio::Poll::new().unwrap();
-
-    let mut events = mio::Events::with_capacity(1024);
-
-    poll.registry().register(&mut listener, mio::Token(0), mio::Interest::READABLE).unwrap();
-
-    let mut streams: std::collections::HashMap<usize, (mio::net::TcpStream, rustls::server::ServerConnection)> = std::collections::HashMap::new();
-    let mut inc: usize = 1;
+    let acceptor = tokio_rustls::TlsAcceptor::from(Arc::from(config));
+    let listener = net::TcpListener::bind(&addr).await?;
 
     loop {
-        poll.poll(&mut events, None).unwrap();
+        let (stream, peer_addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+        let stream = acceptor.accept(stream).await?;
 
-        for event in &events {
-            match event.token() {
-                mio::Token(0) => {
-                    let (mut stream, _) = listener.accept().unwrap();
-                    let tls_conn = rustls::server::ServerConnection::new(config.clone()).unwrap();
-                    poll.registry().register(&mut stream, mio::Token(inc), mio::Interest::READABLE | mio::Interest::WRITABLE).unwrap();
-                    streams.insert(inc, (stream, tls_conn));
-                    inc += 1;
-                },
-                mio::Token(n) => {
-                    let mut shutdown = false;
-
-                    if event.is_writable() {
-                        let (stream, tls) = streams.get_mut(&n).unwrap();
-                        tls.write_tls(stream).unwrap();
-                    }
-
-                    if event.is_readable() {
-                        let (stream, tls) = streams.get_mut(&n).unwrap();
-                        let mut buf = [0u8; 1024];
-
-                        tls.read_tls(stream).unwrap();
-                        tls.process_new_packets().unwrap();
-
-                        let size = match tls.reader().read(&mut buf) {
-                            Ok(sz) => sz,
-                            Err(e) => {
-                                if e.kind() == io::ErrorKind::WouldBlock {
-                                    0
-                                } else if e.kind() == io::ErrorKind::UnexpectedEof {
-                                    poll.registry().deregister(stream).unwrap();
-                                    stream.shutdown(net::Shutdown::Both).unwrap();
-                                    shutdown = true;
-                                    0
-                                } else {
-                                    panic!("{:?}", e);
-                                }
-                            }
-                        };
-
-                        if shutdown {
-                            streams.remove(&n);
-                        } else {
-                            tls.writer().write(&buf[..size]).unwrap();
-                            tls.write_tls(stream).unwrap();
-                        }
-                    }
-                }
+        tokio::spawn(async move {
+            if let Err(err) = handle_connection(stream, peer_addr).await {
+                eprintln!("{:?}", err);
             }
-        }
+        });
     }
+}
+
+async fn handle_connection(stream: tokio_rustls::server::TlsStream<net::TcpStream>, _addr: std::net::SocketAddr) -> io::Result<()> {
+    let (mut reader, mut writer) = tokio::io::split(stream);
+    let _ = tokio::io::copy(&mut reader, &mut writer).await?;
+    writer.flush().await?;
+
+    Ok(())
 }
